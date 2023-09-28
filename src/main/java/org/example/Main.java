65366404aba5e5de5b99d77a0ee54152a9efc91f
Inna-Ser <serebryakovainna@mail.ru>
2023-09-28 11:00:05 -0500

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static boolean isPalindrome(String str) {
+        if (str.replaceAll("\\p{Punct} |\\s", "")
+                .equalsIgnoreCase(new StringBuilder(str.replaceAll("\\p{Punct} |\\s", ""))
+                        .reverse().toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String a = "А роза упала на лапу Азора";
+        String b = "Ночевала тучка золотая";
+
+        System.out.println(isPalindrome(b));
+    }
+
+
+}
